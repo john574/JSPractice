6320cc11a0369240adaab526121ebbd2b9862399
John_Art@ <57656517+john574@users.noreply.github.com>
2025-08-25 13:47:56 +0000

    few code

--- frontendMovie/src/App.jsx
+++ frontendMovie/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-import MovieCard from "./Components/MovieCard"
+
 
 function App() {
+  const movieNumber = 1;
+
   return (
     <>
-      <MovieCard movie={{ title: "Tim's Film", release_date: "2024", url: "https://via.placeholder.com/150" }} />
+     
+
     </>
-  )
+  );
 }
 
 export default App;
